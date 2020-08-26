5f4535077ef3e5460e263a54129d7f47bdd097f2
mikhail <m.s.morozoff@gmail.com>
2020-08-26 12:08:35 +0300

    13

--- src/buildTenders/Tenderers.java
+++ src/buildTenders/Tenderers.java
@@ -1,12 +1,25 @@
 package buildTenders;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 
 public class Tenderers {
     private LinkedList<Team> tenderers = new LinkedList<>();
 
     public void addTeamToTender(Team team) {
         tenderers.add(team);
+    }
+
+    public Map<Skills, Integer> infoOfTeam (Team team){
+        Map<Skills, Integer> infoOfTeam = new HashMap<>();
+        infoOfTeam.put(Skills.PAINT, team.numberOfPaint());
+        infoOfTeam.put(Skills.BUILD_FLOOR, team.numberOfBuildFloor());
+        infoOfTeam.put(Skills.BUILD_WALL, team.numberOfBuildWall());
+        infoOfTeam.put(Skills.BUILD_ROOF, team.numberOfBuildRoof());
+        infoOfTeam.put(Skills.PLUMBER, team.numberOfPlumber());
+        infoOfTeam.put(Skills.ELECTRICIAN, team.numberOfElectrian());
+        return infoOfTeam;
     }
 
 
